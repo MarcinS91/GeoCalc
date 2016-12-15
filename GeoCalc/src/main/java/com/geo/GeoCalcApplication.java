9c673e7ef03e4946a5086a732af89e955f47f17f
MarcinS91 <szewc.marcin91@gmail.com>
2016-12-15 22:51:23 +0100

    init commit

---
+++ GeoCalc/src/main/java/com/geo/GeoCalcApplication.java
@@ -0,0 +1,12 @@
+package com.geo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoCalcApplication.class, args);
+	}
+}
